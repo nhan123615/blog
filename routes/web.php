10e313f3ea8f18c9ece43de6de8ee6b8801aa852
nhan123615 <nhan123615@users.noreply.github.com>
2020-09-21 06:23:15 +0700

    21.09.20

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\User\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +12,27 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//User Routes
+123
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/', function () {
-    return view('user/home');
-});
+/* Route::get('/', [HomeController::class, 'index']); */
+/* Route::group(['namespace' => 'User'],function () {
+    Route::get('/', [HomeController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index'])->name('post');
+}); */
+
+
+/* 
+Route::group(['namespace' => 'Admin'],function () {
+    Route::get('admin/home','HomeController@index')->name('admin/home');
+    Route::resource('admin/post', 'PostController');
+    Route::resource('admin/tag', 'TagController');
+    Route::resource('admin/category', 'CategoryController');
+}); */
+
+/* Route::get('admin/home','App\Http\Controllers\Admin\HomeController@index')->name('admin/home');
+    Route::resource('admin/post', 'App\Http\Controllers\Admin\PostController');
+    Route::resource('admin/tag', 'App\Http\Controllers\Admin\TagController');
+    Route::resource('admin/category', 'App\Http\Controllers\Admin\CategoryController'); */
+
